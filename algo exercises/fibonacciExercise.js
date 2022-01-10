e4433d926df267e0bc3b483223475d4112466698
Katie Ricks <31962559+kricks@users.noreply.github.com>
2022-01-10 20:49:42 +0000

    fibonacci iterative and recursive

---
+++ algo exercises/fibonacciExercise.js
@@ -0,0 +1,23 @@
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+function fibonacciIterative(n){
+  let sum = [0,1];
+  for (let i = 2; i <= 100; i++) {
+    sum.push(sum[i-1] + sum[i-2]);
+  }
+  return sum[n];
+}
+fibonacciIterative(10);
+
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+}
+fibonacciRecursive(10)
